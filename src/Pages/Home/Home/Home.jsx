1540080c17e00b503e66b846e4f8ec65010866b7
anikdas400 <anikchandra400@gmail.com>
2023-11-11 12:44:19 +0600

    navbar and footer layout complite

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../Shard/Footer/Footer";
+import Navbar from "../../Shard/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is Home </h2>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
